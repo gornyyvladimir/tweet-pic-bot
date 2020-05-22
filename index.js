f3ee025c56224bf5ea488dcaa46f1bb4abb048db
Vladimir Gornyj <vladimir.gornyj@piano.io>
2020-05-22 18:21:16 +0300

    add set up for heroku

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 require('dotenv').config();
+const express = require('express');
 const { Telegraf } = require('telegraf');
 const startCommand = require('./commands/start');
 const linkHandler = require('./handlers/link');
 const messageHandler = require('./handlers/message');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+// env variables
+const token = process.env.BOT_TOKEN;
+const port = process.env.PORT || 3000;
+const url = process.env.URL || 'https://i-hate-voice-bot.herokuapp.com';
 
+// app init
+const expressApp = express();
+
+// setting up a bot to work with express
+const bot = new Telegraf(token);
+expressApp.use(bot.webhookCallback(`/bot${token}`));
+bot.telegram.setWebhook(`${url}/bot${token}`);
+// for bot working in chats
+bot.telegram.getMe().then(botInfo => {
+  bot.options.username = botInfo.username;
+});
+
+// handle bot commands
 startCommand(bot);
 linkHandler(bot);
 messageHandler(bot);
 
-bot.launch();
+// express server for web
+expressApp.get('/', (req, res) => {
+  res.send('TweetPicBot is working');
+});
+
+expressApp.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on port ${port}`);
+});
