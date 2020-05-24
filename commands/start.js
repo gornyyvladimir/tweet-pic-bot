37317df59094978994d3fd7856bf9be333cf9ad1
Vladimir Gornyj <vladimir.gornyj@piano.io>
2020-05-24 21:58:20 +0300

    test start command

--- commands/start.js
+++ commands/start.js
@@ -1,4 +1,7 @@
 const config = require('../config');
 
-module.exports = bot =>
-  bot.command(['start, help'], ctx => ctx.reply(config.helpMessage, { parse_mode: 'Markdown' }));
+module.exports = bot => {
+  bot.start(ctx => ctx.reply(config.helpMessage, { parse_mode: 'Markdown' }));
+  bot.help(ctx => ctx.reply(config.helpMessage, { parse_mode: 'Markdown' }));
+  bot.command(['help', 'start'], ctx => ctx.reply('hello'));
+};
