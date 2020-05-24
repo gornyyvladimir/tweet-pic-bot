dd750c2283db7d77bc173c5475584ae414096d67
Vladimir Gornyj <vladimir.gornyj@piano.io>
2020-05-24 22:03:26 +0300

    fix start and help commands

--- commands/start.js
+++ commands/start.js
@@ -3,5 +3,4 @@ const config = require('../config');
 module.exports = bot => {
   bot.start(ctx => ctx.reply(config.helpMessage, { parse_mode: 'Markdown' }));
   bot.help(ctx => ctx.reply(config.helpMessage, { parse_mode: 'Markdown' }));
-  bot.command(['help', 'start'], ctx => ctx.reply('hello'));
 };
