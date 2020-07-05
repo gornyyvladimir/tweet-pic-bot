58b4433c00405b540d7c210bdfd9bf94e6683f1b
Vladimir Gornyj <vladimir.gornyj@piano.io>
2020-07-05 17:30:03 +0300

    fix error message

--- utils/sendError.js
+++ utils/sendError.js
@@ -2,9 +2,10 @@ const config = require('../config');
 
 module.exports = (err, ctx) => {
   const adminChatId = process.env.ADMIN_ID;
-  const errorMessage = `Error: ${err.name} ${err.message} 
-  message: ${ctx.message.text} 
-  type: ${ctx.updateType}`;
+  const errorMessage =
+    `error: ${err.name} ${err.message}\n` +
+    `message: ${ctx.message.text}\n` +
+    `type: ${ctx.updateType}\n`;
   // eslint-disable-next-line no-console
   console.log(errorMessage, err);
   ctx.telegram.sendMessage(adminChatId, errorMessage);
